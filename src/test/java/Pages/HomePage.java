58c2e0d14f6af52579cfe912173f9fe91d217b26
keishiablouws <kmblouws@gmail.com>
2025-10-18 14:11:20 +0200

    Created one method

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -7,18 +7,17 @@ import org.openqa.selenium.support.FindBy;
 public class HomePage {
 
     WebDriver driver;
-
+//Locator to verify that you are on the homepage
     @FindBy(id = "overview-hero")
     WebElement homepageTitle_id;
 
+//
     public HomePage(WebDriver driver){
         this.driver=driver;
     }
-
+    // verify method is displayed
     public void verifyHomePageIsDisplayed(){
         homepageTitle_id.isDisplayed();
     }
-
-
 
 }
