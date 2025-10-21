ac517e3380cbcd17c9dbf4845e7703803e524649
Felokwakhe Bethuel Mtshali <DBN3276@DCC.CORP>
2025-10-21 18:36:21 +0200

    Created Login Page

---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,24 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+    WebDriver driver;
+
+    @FindBy(id="login-email") WebElement loginEmail_id;
+    @FindBy(id="login-password") WebElement loginPassword_id;
+    @FindBy(id="login-submit") WebElement loginSubmit_id;
+
+    public LoginPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public void enterLoginEmail(){loginEmail_id.sendKeys("deez@gmail.com");}
+    public void enterPasswordId(){loginPassword_id.sendKeys("012345678");}
+    public void clickLogin(){loginPassword_id.click();}
+}
+
