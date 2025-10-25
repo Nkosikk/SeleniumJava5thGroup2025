6100786cf4eb7e22659e2bf229616694b37b4ac5
Malibongwe <ap.mshumpela@gmail.com>
2025-10-25 19:04:01 +0200

    Created inventoryPage

--- src/test/java/Tests/Base.java
+++ src/test/java/Tests/Base.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import Pages.HomePage;
+import Pages.InventoryPage;
 import Pages.LearningMaterialPage;
 import Pages.LoginPage;
 import Utils.BrowserFactory;
@@ -15,6 +16,6 @@ public class Base {
     HomePage homePage = PageFactory.initElements(driver, HomePage.class);
     LoginPage loginPage= PageFactory.initElements(driver, LoginPage.class);
     LearningMaterialPage learningMaterialPage = PageFactory.initElements(driver, LearningMaterialPage.class);
-
+    InventoryPage inventoryPage = PageFactory.initElements(driver, InventoryPage.class);
 
 }
