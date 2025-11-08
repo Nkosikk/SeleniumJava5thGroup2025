b32266703d442c1848da225255627f45aaf63703
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-11-08 16:13:02 +0200

    added code to format the numeric value from Excel to string

--- src/test/java/Utils/ReadFromFile.java
+++ src/test/java/Utils/ReadFromFile.java
@@ -1,5 +1,8 @@
 package Utils;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
@@ -29,10 +32,13 @@ public class ReadFromFile {
         }
     }
 
+    public static DataFormatter dataFormatter = new DataFormatter();
     static XSSFSheet sheet = workbook.getSheet("login");
     public static String username = sheet.getRow(1).getCell(0).getStringCellValue();
-    public static String password = sheet.getRow(2).getCell(1).getStringCellValue();
-//    public static Integer password = (int) sheet.getRow(2).getCell(1).getNumericCellValue();
-
-
+    public static String password =dataFormatter.formatCellValue(sheet.getRow(2).getCell(1));
+    @Test
+    public void test(){
+        System.out.println("username is "+username);
+        System.out.println("password is "+password);
+    }
 }
