a751ff0e21d3dbd80b992c86efc5e45de7845309
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-11-11 18:30:33 +0200

    called DataFormatter class to access its data

--- src/test/java/Utils/ReadFromFile.java
+++ src/test/java/Utils/ReadFromFile.java
@@ -1,5 +1,6 @@
 package Utils;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
@@ -9,6 +10,7 @@ import java.io.FileInputStream;
 public class ReadFromFile {
 
     private static String testData = System.getProperty("user.dir")+"/src/test/java/TestData/testData.xlsx";
+    public static DataFormatter dataFormatter = new DataFormatter();
 
     static FileInputStream fs;
 
@@ -31,8 +33,7 @@ public class ReadFromFile {
 
     static XSSFSheet sheet = workbook.getSheet("login");
     public static String username = sheet.getRow(1).getCell(0).getStringCellValue();
-    public static String password = sheet.getRow(2).getCell(1).getStringCellValue();
-//    public static Integer password = (int) sheet.getRow(2).getCell(1).getNumericCellValue();
+    public static String password = dataFormatter.formatCellValue(sheet.getRow(2).getCell(1));
 
 
 }
