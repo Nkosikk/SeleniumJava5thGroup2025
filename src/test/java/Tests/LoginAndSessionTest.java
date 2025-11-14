a10553e6d74afc26222ce8cdf5db55ba7682f845
Spencer Soundy <spencersoundy@gmail.com>
2025-11-14 19:57:16 +0200

    added methods

--- src/test/java/Tests/LoginAndSessionTest.java
+++ src/test/java/Tests/LoginAndSessionTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 public class LoginAndSessionTest extends Base {
 
 
+    @Test
     public void verifySuccessfulLogin() {
         homePage.clickLearningMaterial();
         loginPage.enterLoginEmail("soundytest@gmail.com");
@@ -13,5 +14,43 @@ public class LoginAndSessionTest extends Base {
         loginPage.clickLogin();
         learningMaterialPage.verifyHeading();
     }
+
+    @Test(dependsOnMethods = "verifySuccessfulLogin")
+    public void verifyUnSuccessfulLogin() {
+        homePage.clickLearningMaterial();
+        loginPage.enterLoginEmail("soundytest@gmail.com");
+        loginPage.enterPasswordId("123456789");
+        loginPage.clickLogin();
+
+    }
+
+    @Test(dependsOnMethods = "verifyUnSuccessfulLogin")
+    public void verifySuccessfulLoginWithSpaces() {
+        homePage.clickLearningMaterial();
+        loginPage.enterLoginEmail(" soundytest@gmail.com ");
+        loginPage.enterPasswordId(" 12345678 ");
+        loginPage.clickLogin();
+        learningMaterialPage.verifyHeading();
+    }
+
+    @Test(dependsOnMethods = "verifySuccessfulLoginWithSpaces")
+    public void verifyForcedLogOut() {
+        homePage.clickLearningMaterial();
+        loginPage.enterLoginEmail("soundytest@gmail.com");
+        loginPage.enterPasswordId("12345678");
+        loginPage.clickLogin();
+       driver.navigate().back();
+       loginPage.clickLogin();
+    }
+
+    @Test(dependsOnMethods = "verifyForcedLogOut")
+    public void verifyLogOut() {
+        homePage.clickLearningMaterial();
+        loginPage.enterLoginEmail("soundytest@gmail.com");
+        loginPage.enterPasswordId("12345678");
+        loginPage.clickLogin();
+        webAutomationAdvancePage.clickLogOutButton();
+    }
+
 
 }
