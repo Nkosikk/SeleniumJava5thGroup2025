6d8ddb0e2efa368f1ae9f6b4437628b4ccb93a21
DGKAT1 <kgmokhatla@gmail.com>
2025-11-18 13:23:05 +0200

    added folder structure

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -6,30 +6,5 @@ import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
 
-    WebDriver driver;
-
-    @FindBy(id = "overview-hero")
-    WebElement homepageTitle_id;
-
-    @FindBy(id = "nav-btn-practice")
-    WebElement LearningMaterial_id;
-
-    public HomePage(WebDriver driver){
-        this.driver=driver;
-    }
-
-    public void verifyHomePageIsDisplayed(){
-        homepageTitle_id.isDisplayed();
-    }
-
-    public void clickLearningMaterial(){
-        LearningMaterial_id.click();
-
-    }
-
-
-
-
-
 
 }
